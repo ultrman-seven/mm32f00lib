51ee5859317517736e9e8cec3294496ba32db5fe
Leslie <1813180652@qq.com>
2026-09-13 19:39:18 +0800

    硬件iic发送

--- lib/mm32CppWyHAL/wyI2c.cpp
+++ lib/mm32CppWyHAL/wyI2c.cpp
@@ -1,15 +1,155 @@
 #include "wyI2c.hpp"
 #include "reg_common.h"
 #include "core_cm0.h"
+#include "cppHalReg.hpp"
 
 using namespace IIC;
 using namespace GPIO;
+
+static IIC_HardwareMasterObject *_i2cObj[1];
+
 IIC_Object::IIC_Object(const char *sclPin, const char *sdaPin, GPIO::Mode mScl, GPIO::Mode mSda)
     : scl(GpioPin(sclPin, mScl)), sda(GpioPin(sdaPin, mSda))
 {
 }
-IIC_HardwareObject::IIC_HardwareObject(uint8_t num)
+
+#define I2cRegCr_RestartEn 0x0400
+#define I2cRegCr_StopEn 0x0200
+#define I2cRegCr_EmpInt 0x0100
+#define I2cRegCr_StopInt 0x0080
+#define I2cRegCr_SlaveDisable 0x0040
+#define I2cRegCr_RepEn 0x0020
+// #define I2cRegCr_RepEn 0x0000
+#define I2cRegCr_MasterAdd10 0x0010
+#define I2cRegCr_SlaveAdd10 0x0008
+#define I2cRegCr_SpeedFast 0x0004
+#define I2cRegCr_SpeedStd 0x0002
+#define I2cRegCr_MasterEn 0x0001
+
+#define I2cIt_Tx 0x0010
+#define I2cIt_TxEmpty 0x0010
+#define I2cIt_TxAbort 0x0040
+#define I2cIt_StopDet 0x0200
+#define I2cIt_StartDet 0x0400
+// GpioPin nm("a12");
+
+IIC_HardwareMasterObject::IIC_HardwareMasterObject(uint8_t channel, const char *scl, const char *sda, uint16_t clkHighCnt, uint16_t clkLowCnt, uint8_t fastMode)
 {
+    uint16_t cr;
+    uint8_t af = 0xff;
+    uint8_t ch = channel - 1;
+    // nm = 1;
+    this->i2c = I2C1;
+    // RCC
+    RCC->APB1ENR |= (RCC_APB1ENR_I2C1);
+    this->i2c->ENR &= 0xfffe;
+    // _i2cObj[0] = (void*)this;
+    _i2cObj[ch] = this;
+
+    // AF
+    af = GPIO::afTable2afVal(scl, __I2C_Scl_GPIO_AFs[ch]);
+    if (af != 0xff)
+        GPIO::afConfig(scl, af, GPIO::Mode_AF_OD_Floating);
+    af = GPIO::afTable2afVal(sda, __I2C_Sda_GPIO_AFs[ch]);
+    if (af != 0xff)
+        GPIO::afConfig(sda, af, GPIO::Mode_AF_OD_Floating);
+    this->needStop = 1;
+    this->txLen = 0;
+    if (fastMode)
+    {
+        cr = I2cRegCr_RepEn | I2cRegCr_SpeedFast | I2cRegCr_MasterEn | I2cRegCr_SlaveDisable | I2C_CR_EMPINT;
+        this->i2c->CR = cr;
+        this->i2c->FSHR = clkHighCnt;
+        this->i2c->FSLR = clkLowCnt;
+    }
+    else
+    {
+        cr = I2cRegCr_RepEn | I2cRegCr_SpeedStd | I2cRegCr_MasterEn | I2cRegCr_SlaveDisable | I2C_CR_EMPINT;
+        this->i2c->CR = cr;
+        this->i2c->SSHR = clkHighCnt;
+        this->i2c->SSLR = clkLowCnt;
+    }
+    i2c->IC_INTR_MASK &= 0xc000;
+    i2c->IC_RX_TL = 0x00;
+    // i2c->IC_TX_TL = 1;
+    i2c->IC_TX_TL = 0;
+    // this->i2c->IC_INTR_MASK = 0;
+    // i2c->IC_INTR_MASK |= (I2cIt_StartDet | I2cIt_StopDet);
+    i2c->IC_INTR_MASK |= (I2cIt_StopDet);
+
+    NVIC_SetPriority(I2C1_IRQn, 2);
+    NVIC_EnableIRQ(I2C1_IRQn);
+}
+
+uint8_t IIC_HardwareMasterObject::sendAsync(uint8_t add, uint8_t *datPtr, uint32_t len, uint8_t needStop, void (*cbk)(void *), void *arg)
+{
+    this->arg = arg;
+    this->cbk = cbk;
+    this->txBuf = datPtr;
+    this->txLen = len;
+
+    if (this->needStop)
+    {
+        this->i2c->ENR &= 0xfffe;
+        // this->i2c->CR |= I2cRegCr_RepEn;
+        this->i2c->TAR = add;
+        this->i2c->ENR |= 0x01;
+        // this->i2c->DR = (datPtr[0]);
+    }
+    this->needStop = needStop;
+    this->i2c->IC_INTR_MASK |= (I2cIt_TxEmpty);
+    return 0;
+}
+void IIC::_irqHandle(IIC_HardwareMasterObject *handle)
+{
+    // if (handle->i2c->ISR & I2cIt_StartDet)
+    // {
+    //     handle->i2c->IC_CLR_START_DET;
+    // }
+    if (handle->i2c->ISR & I2cIt_TxEmpty)
+    {
+        if (handle->txLen)
+        {
+            uint8_t tx;
+            tx = *((handle->txBuf)++);
+            if ((handle->needStop) && (1 == handle->txLen))
+            {
+                handle->i2c->CR |= I2cRegCr_StopEn;
+            }
+            --(handle->txLen);
+            handle->i2c->DR = tx;
+        }
+        else
+        {
+            handle->i2c->IC_INTR_MASK &= (uint16_t)(~I2cIt_TxEmpty);
+            // handle->i2c->IC_INTR_MASK &= (uint16_t)(0xDFEF);
+            if (!(handle->needStop))
+            {
+                if (handle->cbk)
+                    handle->cbk(handle->arg);
+            }
+        }
+    }
+
+    // if ((handle->i2c->ISR & I2cIt_TxAbort))
+    if ((handle->i2c->ISR & I2cIt_StopDet))
+    {
+        volatile uint16_t foo;
+        foo = (handle->i2c->IC_CLR_STOP_DET) & I2C_TX_ABRT;
+        handle->i2c->CR &= (uint16_t)(~(I2cRegCr_StopEn));
+
+        handle->i2c->ENR &= 0xfffe;
+        if (handle->cbk)
+            handle->cbk(handle->arg);
+    }
+}
+
+extern "C"
+{
+    void I2C1_IRQHandler(void)
+    {
+        _irqHandle(_i2cObj[0]);
+    }
 }
 
 static inline void __iicDelay(void)
@@ -138,6 +278,34 @@ uint8_t IIC_Object::sendSameByte(uint8_t add, uint8_t reg, uint8_t len, uint8_t 
     this->stop();
     return 0;
 }
+
+uint8_t IIC_Object::send(uint8_t add, uint8_t *txData, uint32_t len, uint8_t start, uint8_t stop)
+{
+    if (start)
+    {
+        if (!this->start())
+            return 1;
+        this->sendByte(add << 1);
+        if (!this->waitAck())
+        {
+            this->stop();
+            return 1;
+        }
+    }
+    while (len--)
+    {
+        this->sendByte(*txData++);
+        if (!this->waitAck())
+        {
+            this->stop();
+            return 1;
+        }
+    }
+    if (stop)
+        this->stop();
+    return 0;
+}
+
 uint8_t IIC_Object::send(uint8_t add, uint8_t reg, uint8_t len, uint8_t *txData)
 {
     if (!this->start())
--- lib/mm32CppWyHAL/wyI2c.hpp
+++ lib/mm32CppWyHAL/wyI2c.hpp
@@ -2,6 +2,7 @@
 #define __MM32_CPP_HAL_WY_LIB_IIC_HPP__
 
 #include "wyGpio.hpp"
+#include "reg_i2c.h"
 
 namespace IIC
 {
@@ -21,6 +22,7 @@ namespace IIC
     public:
         // IIC_Object(const char *scl, const char *sda, bool exPullUp = true);
         IIC_Object(const char *scl, const char *sda, GPIO::Mode mScl = GPIO::Mode_Out_PP, GPIO::Mode mSda = GPIO::Mode_Out_PP);
+        uint8_t send(uint8_t add, uint8_t *txData, uint32_t len, uint8_t start, uint8_t stop);
         uint8_t send(uint8_t add, uint8_t reg, uint8_t len, uint8_t *txData);
         uint8_t sendSameByte(uint8_t add, uint8_t reg, uint8_t len, uint8_t txData);
         uint8_t read(uint8_t add, uint8_t reg, uint8_t len, uint8_t *rxData);
@@ -28,13 +30,24 @@ namespace IIC
         uint8_t send16bitReg(uint8_t add, uint16_t reg, uint8_t len, uint8_t *txData);
     };
 
-    class IIC_HardwareObject
+    class IIC_HardwareMasterObject
     {
     private:
-        /* data */
+        I2C_TypeDef *i2c;
+        void (*cbk)(void *);
+        void *arg;
+        uint32_t txLen;
+        uint8_t *txBuf;
+        uint8_t needStop;
+
+        friend void _irqHandle(IIC_HardwareMasterObject *);
+
     public:
-        IIC_HardwareObject(uint8_t num);
-        // ~IIC_HardwareObject();
+        IIC_HardwareMasterObject(){}
+        IIC_HardwareMasterObject(uint8_t channel, const char *scl, const char *sda, uint16_t clkHighCnt, uint16_t clkLowCnt, uint8_t mode);
+        // void init(uint8_t channel, const char *scl, const char *sda, uint16_t clkHighCnt, uint16_t clkLowCnt, uint8_t mode);
+        uint8_t sendAsync(uint8_t add, uint8_t *datPtr, uint32_t len, uint8_t needStop, void (*cbk)(void *), void *arg);
+        // ~IIC_HardwareMasterObject();
     };
 
 }
