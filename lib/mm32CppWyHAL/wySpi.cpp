66d7b4067fbb64284b5b4405f28687186c5cd66a
ultrman-seven <1813180652@qq.com>
2026-09-16 16:14:06 +0800

    增加了spi的异步读写

--- lib/mm32CppWyHAL/wySpi.cpp
+++ lib/mm32CppWyHAL/wySpi.cpp
@@ -2,6 +2,50 @@
 
 using namespace SPI;
 
+#define SpiCrtState_TxEmpty 0x01
+#define SpiCrtState_Rx 0x02
+#define SpiCrtState_TxFull 0x04
+#define SpiCrtState_Rx4Byte 0x08
+
+#define SpiIntState_TxFifoEmpty 0x01
+#define SpiIntState_RxOk 0x02
+#define SpiIntState_RxOver 0x08
+#define SpiIntState_RxMatch 0x10
+#define SpiIntState_RxFull 0x20
+#define SpiIntState_TxCplt 0x40
+
+#define SpiGenCtrl_Enable 0x01
+#define SpiGenCtrl_Disable 0x00
+
+#define SpiGenCtrl_IntEnable 0x02
+#define SpiGenCtrl_IntDisable 0x00
+
+#define SpiGenCtrl_ModeMaster 0x04
+#define SpiGenCtrl_ModeSlave 0x00
+
+#define SpiGenCtrl_TxEn 0x08
+#define SpiGenCtrl_RxEn 0x10
+
+#define SpiGenCtrl_NssEn 0x0400
+#define SpiGenCtrl_NssToggleEn 0x1000
+
+#define SpiGenCtrl_DataWidth_8bit 0x00
+#define SpiGenCtrl_DataWidth_32bit 0x0800
+
+#define SpiComCtrl_ClkPhaseEdge1 0x0001
+#define SpiComCtrl_ClkPhaseEdge2 0x0000
+
+#define SpiComCtrl_ClkPolarityIdleHigh 0x0002
+#define SpiComCtrl_ClkPolarityIdleLow 0x0000
+
+#define SpiComCtrl_Lsb 0x0004
+#define SpiComCtrl_Msb 0x0000
+
+#define SpiComCtrl_RxEdgeTail 0x0010
+#define SpiComCtrl_TxNo1ClkDelay 0x0020
+
+static SpiObject *_obj[1];
+
 uint8_t SpiObject::readWrite(uint8_t dat)
 {
     uint32_t ret;
@@ -16,39 +60,64 @@ uint8_t SpiObject::readWrite(uint8_t dat)
     return ret & 0x00ff;
 }
 
-void SpiObject::readWrite(uint8_t *txBuf, uint8_t *rxBuf, uint32_t len)
+void SpiObject::readWriteAsync(uint8_t *txBuf, uint8_t *rxBuf, uint32_t len, void (*cbk)(void *), void *arg)
 {
-    uint32_t ret[4];
-    // while (len--)
-    // {
-        // spi->TDR = *txBuf++;
-        // spi->TDR = 0;
+    this->cbk = cbk;
+    this->arg = arg;
 
-        // while (!(spi->SR & 1))
-        //     ;
-        // while (!(spi->SR & 2))
-        //     ;
-        ret[0] = spi->RDR;
-        // spi->TDR = 0;
-        ret[1] = spi->RDR;
-        // spi->TDR = 0;
-        ret[2] = spi->RDR;
-        // spi->TDR = 0;
-        ret[3] = spi->RDR;
-        // (*rxBuf++) = ((spi->RDR) & 0x00ff);
-        rxBuf[3] = (ret[0] & 0x00ff);
-        rxBuf[2] = (ret[1] & 0x00ff);
-        rxBuf[1] = (ret[2] & 0x00ff);
-        rxBuf[0] = (ret[3] & 0x00ff);
-    // }
+    this->txBuf = txBuf;
+    this->rxBuf = rxBuf;
+    this->txLen = len;
+    this->rxLen = len;
+
+    this->spi->GCTL |= (SpiGenCtrl_RxEn | SpiGenCtrl_TxEn | SpiGenCtrl_IntEnable);
+    this->spi->INTEN |= (SpiIntState_RxOk | SpiIntState_TxFifoEmpty);
+    this->spi->GCTL |= SpiGenCtrl_Enable;
+}
+
+void SpiObject::readAsync(uint8_t *buf, uint32_t len, void (*cbk)(void *), void *arg)
+{
+    this->cbk = cbk;
+    this->arg = arg;
+
+    this->rxBuf = buf;
+    this->txBuf = nullptr;
+    this->txLen = 0;
+    this->rxLen = len;
+
+    this->spi->GCTL &= ((uint32_t)(~(SpiGenCtrl_TxEn)));
+    this->spi->GCTL |= (SpiGenCtrl_RxEn | SpiGenCtrl_IntEnable);
+    this->spi->RXDNR = 1;
+    this->spi->INTEN |= SpiIntState_RxOk;
+    this->spi->GCTL |= SpiGenCtrl_Enable;
+}
+
+void SpiObject::writeAsync(uint8_t *buf, uint32_t len, void (*cbk)(void *), void *arg)
+{
+    this->cbk = cbk;
+    this->arg = arg;
+
+    this->txBuf = buf;
+    this->rxBuf = nullptr;
+    this->txLen = len;
+    this->rxLen = 0;
+
+    this->spi->GCTL &= ((uint32_t)(~(SpiGenCtrl_RxEn)));
+    this->spi->GCTL |= (SpiGenCtrl_TxEn);
+
+    this->spi->INTEN |= (SpiIntState_TxCplt | SpiIntState_TxFifoEmpty);
+    this->spi->GCTL |= SpiGenCtrl_Enable;
+    this->spi->GCTL |= SpiGenCtrl_IntEnable;
 }
 
 #include "reg_rcc.h"
-SpiObject::SpiObject(uint8_t num, const char *sclk, const char *mosi, const char *miso)
+SpiObject::SpiObject(uint8_t num, const char *sclk, const char *mosi, const char *miso, GPIO::Mode mosiMode, uint16_t clkDiv, uint8_t clkCfg)
 {
     // clock 4 spi
     RCC->APB1ENR |= RCC_APB1ENR_SPI1;
     spi = SPI1;
+
+    _obj[0] = this;
 
     // gpio af
     if (nullptr != sclk)
@@ -57,29 +126,71 @@ SpiObject::SpiObject(uint8_t num, const char *sclk, const char *mosi, const char
     }
     if (nullptr != mosi)
     {
-        GPIO::afConfig(mosi, 0, GPIO::Mode_AF_PP);
+        GPIO::afConfig(mosi, 0, mosiMode);
     }
     if (nullptr != miso)
     {
         GPIO::afConfig(miso, 0, GPIO::Mode_IN_FLOATING);
-        // spi->GCR |= 0x10;
     }
 
-    // spi cfg
-    spi->GCR &= ~((0x01U << (11)));
-    // spi->GCR; // nss,mode
-    spi->CCR |= 1; // lsb,cpol,cpha
-    spi->CCR |= 2;
-    spi->CCR |= ((0x01U << (3)));
+    spi->GCR &= (uint32_t)(~(SpiGenCtrl_DataWidth_32bit));
 
-    spi->BRR = 8;
-    // spi->BRR = 14;
+    spi->GCR |= SpiGenCtrl_ModeMaster;
+
+    spi->CCR |= (clkCfg);
+    spi->BRR = clkDiv;
 
     spi->EXTCTL = 8;
+}
 
-    // rx tx enable
-    (spi->GCR) |= ((0x01U << (4)));
-    (spi->GCR) |= ((0x01U << (3)));
-    // spi enable
-    spi->GCR |= 1;
+namespace SPI
+{
+    void spiIrqHandle(SpiObject *handle)
+    {
+        if ((handle->spi->INTSTAT) & SpiIntState_TxFifoEmpty)
+        {
+            handle->spi->TDR = *(handle->txBuf);
+            --(handle->txLen);
+            if (0 == (handle->txLen))
+            {
+                handle->spi->INTEN &= ((uint32_t)(~(SpiIntState_TxFifoEmpty)));
+            }
+        }
+
+        if ((handle->spi->INTSTAT) & SpiIntState_TxCplt)
+        {
+            (handle->spi->INTCLR) = SpiIntState_TxCplt;
+            if (0 == (handle->txLen))
+            {
+                handle->spi->INTEN &= ((uint32_t)(~(SpiIntState_TxCplt)));
+                handle->spi->GCTL &= ((uint32_t)(~(SpiGenCtrl_Enable | SpiGenCtrl_RxEn | SpiGenCtrl_TxEn | SpiGenCtrl_IntEnable)));
+                if (handle->cbk)
+                    handle->cbk(handle->arg);
+            }
+        }
+
+        if ((handle->spi->INTSTAT) & SpiIntState_RxOk)
+        {
+            (handle->spi->INTCLR) = SpiIntState_RxOk;
+            *(handle->rxBuf) = (handle->spi->RDR);
+            --(handle->rxLen);
+            ++(handle->rxBuf);
+            if (0 == (handle->rxLen))
+            {
+                handle->spi->GCTL &= ((uint32_t)(~(SpiGenCtrl_Enable | SpiGenCtrl_RxEn | SpiGenCtrl_IntEnable)));
+                handle->spi->INTEN &= ((uint32_t)(~(SpiIntState_RxOk)));
+
+                if (handle->cbk)
+                    handle->cbk(handle->arg);
+            }
+        }
+    }
+} // namespace SPI
+
+extern "C"
+{
+    void SPI1_IRQHandler(void)
+    {
+        spiIrqHandle(_obj[0]);
+    }
 }
--- lib/mm32CppWyHAL/wySpi.hpp
+++ lib/mm32CppWyHAL/wySpi.hpp
@@ -4,18 +4,41 @@
 #include "stdint.h"
 #include "reg_spi.h"
 #include "wyGpio.hpp"
+
+#define __Spi_ClkCfg_PhaseEdge1 0x0001
+#define __Spi_ClkCfg_PhaseEdge2 0x0000
+#define __Spi_ClkCfg_PolarityIdleHigh 0x0002
+#define __Spi_ClkCfg_PolarityIdleLow 0x0000
+
+#define Spi_ClkCfg_Edge1High (__Spi_ClkCfg_PhaseEdge1 | __Spi_ClkCfg_PolarityIdleHigh)
+#define Spi_ClkCfg_Edge2High (__Spi_ClkCfg_PhaseEdge2 | __Spi_ClkCfg_PolarityIdleHigh)
+#define Spi_ClkCfg_Edge1Low (__Spi_ClkCfg_PhaseEdge1 | __Spi_ClkCfg_PolarityIdleLow)
+#define Spi_ClkCfg_Edge2Low (__Spi_ClkCfg_PhaseEdge2 | __Spi_ClkCfg_PolarityIdleLow)
+
 namespace SPI
 {
     class SpiObject
     {
     private:
         SPI_TypeDef *spi;
+        void (*cbk)(void *);
+        void *arg;
+        uint8_t *txBuf;
+        uint8_t *rxBuf;
+        uint32_t txLen;
+        uint32_t rxLen;
+        friend void spiIrqHandle(SpiObject *handle);
 
     public:
-        SpiObject(uint8_t num, const char *sclk, const char *mosi, const char *miso = nullptr);
+        SpiObject(uint8_t num, const char *sclk,
+                  const char *mosi = nullptr, const char *miso = nullptr,
+                  GPIO::Mode mosiMode = GPIO::Mode_AF_PP,
+                  uint16_t clkDiv = 8, uint8_t clockCfg);
         // ~SpiObject();
         uint8_t readWrite(uint8_t);
-        void readWrite(uint8_t *txBuf, uint8_t *rxBuf, uint32_t len);
+        void readWriteAsync(uint8_t *txBuf, uint8_t *rxBuf, uint32_t len, void (*cbk)(void *), void *arg);
+        void readAsync(uint8_t *buf, uint32_t len, void (*cbk)(void *), void *arg);
+        void writeAsync(uint8_t *buf, uint32_t len, void (*cbk)(void *), void *arg);
     };
 
 } // namespace SPI
